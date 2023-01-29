3cc6430857f837dba5d5d09672a5cfcfca35d6c9
Johnny Nguyen <jnguyen@int.bgwgroup.com.au>
2023-01-29 16:24:07 +1000

    finish section 3

--- src/Components/Section3/ThreeRight.jsx
+++ src/Components/Section3/ThreeRight.jsx
@@ -49,61 +49,61 @@ export const ThreeRight = () => {
             </div>
             <li className="right-li1">
               <div className="solution-btn">
-                <FontAwesomeIcon className="solution-icon" icon={faCertificate} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faCertificate} color="#d13939"/>  
                 <p>Exceptional Service</p>
               </div>
             </li>
             <li className="right-li2">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faTruck} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faTruck} color="#d13939"/>  
                 <p>Supplier Consolidation</p>
               </div>
             </li>
             <li className="right-li3">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faCartShopping} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faCartShopping} color="#d13939"/>  
                 <p>Simplified Procurement</p>
               </div>
             </li>
             <li className="right-li4">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faShare} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faShare} color="#d13939"/>  
                 <p>National Distribution Network</p>
               </div>
             </li>
             <li className="right-li5">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faLaptopCode} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faLaptopCode} color="#d13939"/>  
                 <p>E-Commerce Capability</p>
               </div>
             </li>
             <li className="right-li6">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faLightbulb} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faLightbulb} color="#d13939"/>  
                 <p>Sustainability and Innovation</p>
               </div>
             </li>
             <li className="right-li7">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faLink} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faLink} color="#d13939"/>  
                 <p>Sophisticated Supply Chain Solutions</p>
               </div>
             </li>
             <li className="right-li8">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faGlobe} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faGlobe} color="#d13939"/>  
                 <p>Global Expertise</p>
               </div>
             </li>
             <li className="right-li9">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faFileShield} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faFileShield} color="#d13939"/>  
                 <p>Quality and Safety</p>
               </div>
             </li>
             <li className="right-li10">
               <div className="solution-btn">
-                <FontAwesomeIcon icon={faChartColumn} size="2x" color="#d13939"/>  
+                <FontAwesomeIcon icon={faChartColumn} color="#d13939"/>  
                 <p>Customised Reporting</p>
               </div>
             </li>
