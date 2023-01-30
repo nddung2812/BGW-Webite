b48eed6e83a65e9f4c9bc51263d8b4bd227385fe
jojo_nguyen <jnguyen@bgwgroup.com.au>
2023-01-30 14:30:33 +1000

    test

--- src/Components/Section4/ProjectCarousel.jsx
+++ src/Components/Section4/ProjectCarousel.jsx
@@ -28,74 +28,73 @@ export const ProjectCarousel = ({ activated, setActivated }) => {
   return (
     <div className='project-carousel-ctn'>
         <div className="main-carousel">
-        <Swiper
-          spaceBetween={10}
-          centeredSlides={true}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Pagination, Autoplay, Navigation]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
+          <Swiper
+            spaceBetween={10}
+            centeredSlides={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Autoplay, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=1" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=2" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=3" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=4" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=5" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=6" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=7" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="test">
+                <p>test</p>
+              </div>
+              <img src="https://picsum.photos/1600/800?random=8" alt="" />
+            </SwiperSlide>
+          </Swiper>
+          <div className="close-industry-btn" onClick={handleClick}>
+            <div className="xmark">
+              <FontAwesomeIcon icon={faXmark}  />
             </div>
-            <img src="https://picsum.photos/1600/800?random=1" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=2" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=3" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=4" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=5" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=6" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=7" alt="" />
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="test">
-              <p>test</p>
-            </div>
-            <img src="https://picsum.photos/1600/800?random=8" alt="" />
-          </SwiperSlide>
-       
-        </Swiper>
-        </div>
-        <div className="close-industry-btn" onClick={handleClick}>
-          <div className="xmark">
-            <FontAwesomeIcon icon={faXmark}  />
           </div>
         </div>
     </div>
